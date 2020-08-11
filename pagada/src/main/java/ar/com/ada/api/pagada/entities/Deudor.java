92c17a923f585a6aba431468cf6077f133631d75
erikagarcia29 <erikagarciasierra@gmail.com>
2020-08-10 21:40:14 -0300

    Se comienza el proyecto pagADA:
    - Se crea la estrucura del proyecto con 4 dependencias principales: Spring Web, Spring JPA, H2 Databae, y MySQL database.
    - Se crea la estrucutra de paquetes: controllres, models, repos y services
    - Se crean las entidades Empresa y Deudor. Para el caso de Pais se genero una Clase con enumerados, pero en Empresa y Deudor vamos a referenciarlo por Id(codigo ISO). Para los tiposDocumentosId vamos a referenciarlo como ENUMS en orden, que coinciden con la numeracion de la base de datos.
    Se comenzo a trabajar con la entidad servicio, que tendra relacion con Empresa y Deudor.
    
    Co-authored-by: Primave
    Co-authored-by: Erikagarcia29
    Co-authored-by: AilinCaminos
    Co-authored-by: Sofia
    Co-authored-by: meiishulen

---
+++ pagada/src/main/java/ar/com/ada/api/pagada/entities/Deudor.java
@@ -0,0 +1,27 @@
+package ar.com.ada.api.pagada.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import ar.com.ada.api.pagada.entities.Pais.TipoIdImpositivoEnum;
+
+@Entity
+@Table(name = "deudor")
+public class Deudor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "deudor_id")
+    private Integer deudorId;
+    @Column(name = "pais_id")
+    private Integer paisId;
+    @Column(name = "tipo_id_impositivo_id")
+    private TipoIdImpositivoEnum tipoIdImpositivo;
+    @Column(name = "id_impositivo")
+    private String idImpositivo;
+    private String nombre;
+} 
---
+++ pagada/src/main/java/ar/com/ada/api/pagada/entities/Servicio.java
@@ -0,0 +1,8 @@
+package ar.com.ada.api.pagada.entities;
+
+public class Servicio {
+    private Integer servicioId;
+    private Empresa empresaId;
+    private Deudor deudorId;
+
+}
