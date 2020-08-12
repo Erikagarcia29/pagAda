591edccb102585d8f5fdc0451b1c3b22e52b94ec
erikagarcia29 <erikagarciasierra@gmail.com>
2020-08-12 16:45:39 -0300

    Deudor: agregan el controller de Deudor para crear deudores usando el… … service y repo. En este caso se uso la otra forma que es a partir de los parametros del deudor, crear una entidad deudor, no crearla si no esta completo o hay algun otro problema.
    Co-authored-by: Alexmary
    Co-authored-by: meiishulen
    Co-authored-by: AilinCaminos
    Co-authored-by: Erikagarcia29
    Co-authored-by: Sofia

---
+++ pagada/src/main/java/ar/com/ada/api/pagada/controllers/DeudorController.java
@@ -0,0 +1,60 @@
+package ar.com.ada.api.pagada.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import ar.com.ada.api.pagada.entities.Deudor;
+import ar.com.ada.api.pagada.models.response.GenericResponse;
+
+@RestController
+public class DeudorController {
+    @Autowired
+
+    DeudorService deudorService;
+
+    @GetMapping("/api/deudores")
+
+    public ResponseEntity<List<Deudor>> obtenerDeudores() {
+
+        List<Deudor> deudores = deudorService.listarDeudores();
+
+        return ResponseEntity.ok(deudores);
+
+    }
+
+    @PostMapping("/api/deudores")
+
+    public ResponseEntity<GenericResponse> crearDeudor(@RequestBody DeudorRequest dr) {
+
+        GenericResponse gr = new GenericResponse();
+
+        //
+
+        Deudor deudor = deudorService.crearDeudor(dr.paisId, dr.tipoIdImpositivo, dr.idImpositivo, dr.nombre);
+
+        if (deudor != null) {
+
+            gr.isOk = true;
+
+            gr.id = deudor.getDeudorId();
+
+            gr.message = "Deudor cargado con éxito.";
+
+            return ResponseEntity.ok(gr);
+
+        }
+
+        gr.isOk = false;
+
+        gr.message = "No se pudo cargar al Deudor.";
+
+        return ResponseEntity.badRequest().body(gr);
+
+    }
+}
