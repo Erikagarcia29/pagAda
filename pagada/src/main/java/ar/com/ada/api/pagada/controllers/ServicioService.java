5afca261dc938a915343a0710a0b1b06a4bf4435
erikagarcia29 <erikagarciasierra@gmail.com>
2020-08-13 20:08:17 -0300

    se completa el codigo de la clase servicioService

--- pagada/src/main/java/ar/com/ada/api/pagada/controllers/ServicioService.java
+++ pagada/src/main/java/ar/com/ada/api/pagada/controllers/ServicioService.java
@@ -1,5 +1,42 @@
 package ar.com.ada.api.pagada.controllers;
 
-public class ServicioService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import ar.com.ada.api.pagada.entities.TipoServicio;
+import ar.com.ada.api.pagada.repos.TipoServicioRepository;
+
+public class ServicioService {
+    @Autowired
+
+    TipoServicioRepository tSRepository;
+
+    public List<TipoServicio> listarTipoServicios() {
+
+        return tSRepository.findAll();
+
+    }
+
+    public Integer sumar(int num1, int num2) {
+
+        int resultado;
+
+        resultado = num1 + num2;
+
+        return resultado;
+
+    }
+
+    public boolean crearTipoServicio(TipoServicio tipo) {
+
+        if (tSRepository.existsById(tipo.getTipoServicioId()))
+
+            return false;
+
+        tSRepository.save(tipo);
+
+        return true;
+
+    }
 }
